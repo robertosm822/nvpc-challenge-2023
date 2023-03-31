72b80277f82270b96eac7f459cef8a5188eb5900
Roberto Soares <roberto.melo@businessintegrator.com.br>
2023-03-31 00:01:00 -0300

    Melhoria em ordenar por datas de push

---
+++ functions.js
@@ -0,0 +1,96 @@
+function initHtml(){
+    let htmlListRepos = `<ul class="list-group">`;
+    for(let i=0; i < reposRobertoSm822.length; i++){
+        htmlListRepos += `<li class="list-group-item">
+            <b>Nome do repositório:</b> ${reposRobertoSm822[i].name}<br>
+            <b>Nome completo: </b>${reposRobertoSm822[i].full_name}<br>
+            <b>Link do repositório: </b><a href="${reposRobertoSm822[i].html_url}" target="_blank">${reposRobertoSm822[i].html_url}</a><br>
+            <b>Descrição: </b>${reposRobertoSm822[i].description}<br>
+            <b>Último Push: </b>${localeDate(reposRobertoSm822[i].pushed_at.toLocaleString())}
+        </li>`; 
+        
+    }
+    htmlListRepos += `<ul>`;
+    jQuery(document).ready(function() {    
+        jQuery("#prository-list").html(htmlListRepos);
+        jQuery("#total-repos").html(reposRobertoSm822.length);
+    });
+}
+function orderAsc(){
+    let ascOrder = reposRobertoSm822.sort( function ( a, b ) { return b.name - a.name; } );
+    let html = showHtmlUl(ascOrder);
+    jQuery("#prository-list").html(html);
+    //console.log(ascOrder);
+    return ascOrder;
+}
+function toTimestamp(strDate){
+    var datum = Date.parse(strDate);
+    return datum/1000;
+}
+function orderByPush(){
+    let ascOrder = reposRobertoSm822.sort( function ( a, b ) { console.log(toTimestamp(b.pushed_at)); return toTimestamp(b.pushed_at) - toTimestamp(a.pushed_at); } );
+    ascOrder = ascOrder.reverse();
+    let html = showHtmlUl(ascOrder);
+    jQuery("#prository-list").html(html);
+    //console.log(ascOrder);
+    return ascOrder;
+}
+function orderByPushDesc(){
+    let ascOrder = reposRobertoSm822.reverse( function ( a, b ) {  return toTimestamp(b.pushed_at) - toTimestamp(a.pushed_at); } );
+    //ascOrder = ascOrder.reverse();
+    let html = showHtmlUl(ascOrder);
+    jQuery("#prository-list").html(html);
+    //console.log(ascOrder);
+    return ascOrder;
+}
+function orderDesc(data){
+    let descOrder = reposRobertoSm822.sort().reverse();
+    let html = showHtmlUl(descOrder);
+    jQuery("#prository-list").html(html);
+    //console.log(descOrder);
+    return descOrder;
+}
+function localeDate(data){
+    var d = new Date(data); 
+    //console.log( d.toLocaleString() ); 
+    return d.toLocaleString();
+}
+function showHtmlUl(dados){
+    
+    let htmlListRepos = `<ul class="list-group">`;
+        for(let i=0; i < dados.length; i++){
+            htmlListRepos += `<li class="list-group-item">
+                <b>Name:</b> ${dados[i].name}<br>
+                <b>Full Name: </b>${dados[i].full_name}<br>
+                <b>Link Repo: </b><a href="${dados[i].html_url}" target="_blank">${reposRobertoSm822[i].html_url}</a><br>
+                <b>Description: </b>${dados[i].description}<br>
+                <b>Último Push: </b>${localeDate(dados[i].pushed_at)}
+            </li>`; 
+            
+        }
+        htmlListRepos += `<ul>`;
+    return htmlListRepos;
+}
+function searchRepos(_this){
+    //console.log(_this.value);
+    // Get the array of keys
+    var keys = Object.keys( reposRobertoSm822 );
+
+    // Sort the keys in descending order
+    keys.sort( function ( a, b ) { return b - a; } );
+
+    let newRepoSearch = [];
+    // Iterate through the array of keys and access the corresponding object properties
+    for ( var i = 0; i < keys.length; i++ ) {
+        //console.log( keys[i], reposRobertoSm822[ keys[i] ] );
+        if(reposRobertoSm822[ keys[i] ].name.includes(_this.value) ){
+            //console.log( keys[i], reposRobertoSm822[ keys[i] ] );
+            newRepoSearch.push(reposRobertoSm822[ keys[i] ]);   
+        }
+    }
+    //rendenrizar a tela com a busca sugestionada
+    let html = showHtmlUl(newRepoSearch);
+    jQuery("#prository-list").html(html);
+    jQuery("#total-repos").html(newRepoSearch.length);
+ 
+}
